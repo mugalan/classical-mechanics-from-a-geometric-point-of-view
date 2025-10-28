1b7423bc369932b596806678c2d42ea113728ee8
Sanjeeva Maithripala <77828283+mugalan@users.noreply.github.com>
2025-10-28 11:52:00 +0530

    1216

--- sims/rigid_body_sim.py
+++ sims/rigid_body_sim.py
@@ -1213,7 +1213,7 @@ class RigidBodySim:
 
         # H using e1 & e3
         e1 = np.array([1., 0., 0.])
-        e3 = np.array([0., 0., 1.])
+        e3 = np.array([0., 0., -1.])
         H1 = -self.hat_matrix(R.T @ e1)  # == -(R^T @ hat(e1) @ R)
         H3 = -self.hat_matrix(R.T @ e3)
         H_km1 = np.vstack([H1, H3])    # (6,3)
